e96fecc07aa72d4a825c077db31d879f8779e553
Your Name <you@example.com>
2025-10-07 17:54:06 +0200

    chore: style active-link class

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -8,12 +8,12 @@ defineProps<{
   links: linkProp[]
 }>()
 </script>
-
 <template>
   <RouterLink
     v-for="link in links"
     :key="link.title"
     :to="link.to"
+    active-class="text-primary bg-muted"
     class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
   >
     <iconify-icon :icon="link.icon"></iconify-icon>
