fe9aa1c7155d7672a9e8c62fd17869b67540eeaf
Your Name <you@example.com>
2025-10-07 17:24:26 +0200

    feat: Sidebar component feat: TopNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import Sidebar from '@/components/Layout/Sidebar.vue'
+</script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
