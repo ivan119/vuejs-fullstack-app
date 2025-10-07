d63dcef55f305939a2320389f75e2103f95372a7
Your Name <you@example.com>
2025-10-07 18:06:57 +0200

    feat: add default layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import TopNavbar from '@/components/Layout/TopNavbar.vue'
-import Sidebar from '@/components/Layout/Sidebar.vue'
+import DefaultLayout from '@/components/Layout/main/default.vue'
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <DefaultLayout>
+    <RouterView />
+  </DefaultLayout>
 </template>
