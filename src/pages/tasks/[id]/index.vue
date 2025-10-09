4c07895799a4974e6ec2e16b983e71cea5c312c8
Your Name <you@example.com>
2025-10-09 12:14:23 +0200

    feat: single task page

---
+++ src/pages/tasks/[id]/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute, type RouteLocationNormalizedLoaded } from 'vue-router'
+type ProjectRoute = RouteLocationNormalizedLoaded & {
+  params: {
+    id: string
+  }
+}
+const route = useRoute() as ProjectRoute
+</script>
+
+<template>
+  <h1>Single Task View {{ route.params?.id }}</h1>
+</template>
