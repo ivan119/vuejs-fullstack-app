b07b08c315862ec3edc870910c02db0380771373
Your Name <you@example.com>
2025-10-09 12:12:26 +0200

    feat: handle click on DataTable

--- src/composables/DataTableHeaders.ts
+++ src/composables/DataTableHeaders.ts
@@ -1,11 +1,12 @@
 import { h, ref } from 'vue'
 import type { Tables } from '../../database/types.ts'
 import type { ColumnDef } from '@tanstack/vue-table'
+import { RouterLink } from 'vue-router'
 
-export const useDataTableHeaders = <TableName extends keyof Tables<never>>() => {
-  const columns = ref<ColumnDef<Tables<never>[TableName]>[]>([])
+export const useDataTableHeaders = <TableName extends keyof Tables<'projects' | 'tasks'>>() => {
+  const columns = ref<ColumnDef<Tables<'projects' | 'tasks'>[TableName]>[]>([])
 
-  const setColumns = (data: Tables<'projects' | 'tasks'>[], blacklist?: string[]) => {
+  const setColumns = (data: Tables<'projects' | 'tasks'>[], blacklist: string[], to: string) => {
     if (!data || !data.length) return
     let keys = Object.keys(data[0] as Record<TableName, unknown>)
     keys = blacklist ? keys.filter((key) => !blacklist.includes(key)) : keys
@@ -13,12 +14,22 @@ export const useDataTableHeaders = <TableName extends keyof Tables<never>>() => 
       accessorKey: key,
       header: () => h('div', { class: 'text-left capitalize' }, key.replace('_', ' ')),
       cell: ({ row }) => {
-        const value = row.getValue(key)
-        return h(
-          'div',
-          { class: 'text-left font-medium' },
-          Array.isArray(value) ? JSON.stringify(value) : row.getValue(key),
-        )
+        const value = row.getValue(key as string)
+        const { slug, id } = row?.original as never
+        return key === 'name' && (slug || id)
+          ? h(
+              RouterLink,
+              {
+                to: `/${to}/${id ?? slug}`,
+                class: 'text-center hover:bg-muted block w-full text-left',
+              },
+              () => row.getValue(key),
+            )
+          : h(
+              'div',
+              { class: 'text-left font-medium' },
+              Array.isArray(value) ? JSON.stringify(value) : row.getValue(key),
+            )
       },
     }))
   }
