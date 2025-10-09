d312cb4de4ab45d9158ebcedabc5efc9b94e0a8c
Your Name <you@example.com>
2025-10-09 12:14:52 +0200

    chore: types

--- src/pages/projects/[id]/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-</script>
-
-<template>
-  <h1>Single Project View {{ route.params?.id }}</h1>
-</template>
---
+++ src/pages/projects/[slug]/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRoute, type RouteLocationNormalizedLoaded } from 'vue-router'
+
+type ProjectRoute = RouteLocationNormalizedLoaded & {
+  params: {
+    slug: string
+  }
+}
+const route = useRoute() as ProjectRoute
+</script>
+
+<template>
+  <h1>Single Project View {{ route.params.slug }}</h1>
+</template>
