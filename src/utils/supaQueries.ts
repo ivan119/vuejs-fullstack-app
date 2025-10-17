eb84ff6e77ccb0cdf56778d26b90cf1fc47c1cc2
Your Name <you@example.com>
2025-10-17 18:24:23 +0200

    feat: setup single project page chore: add single project query

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -6,3 +6,14 @@ export const taskWithProjectsQuery = supabaseClient
 export const projectsWithProjectsQuery = supabaseClient.from('projects').select()
 export type TasksWithProjects = QueryData<typeof taskWithProjectsQuery>
 export type Projects = QueryData<typeof projectsWithProjectsQuery>
+export const projectQuery = (slug: string) =>
+  supabaseClient
+    .from('projects')
+    .select(
+      `*, tasks(
+id,name,status,due_date)`,
+    )
+    .eq('slug', slug)
+    .single()
+
+export type Project = QueryData<ReturnType<typeof projectQuery>>
