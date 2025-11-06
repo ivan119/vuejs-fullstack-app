8200e425d2f7a1750c67b29e73987317a371ab12
Your Name <you@example.com>
2025-11-06 17:19:22 +0100

    chore: add auto import for composables

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       // Include auto-imported packages in Vite's `optimizeDeps` options
       // Recommend to enable
       viteOptimizeDeps: true,
-      dirs: ['src/stores'],
+      dirs: ['src/stores/**', 'src/composables/**'],
     }),
     vue({
       template: {
