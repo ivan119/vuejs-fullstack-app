990f736e7e7825b66855141512d0cf0d98a3bac5
Your Name <you@example.com>
2025-11-06 15:50:38 +0100

    feat: auth routeGourd

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -32,10 +32,16 @@ const bottomLinks = [
     icon: 'lucide:log-out',
   },
 ]
+
+const router = useRouter()
 const executeAction = async (linkTitle: string) => {
   if (linkTitle === 'Sign Out') {
     const { logout } = await import('@/utils/supaAuth')
-    await logout()
+    const isLoggedOut = await logout()
+
+    if (isLoggedOut) {
+      router.push('/login')
+    }
   }
 }
 </script>
