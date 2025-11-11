14667b4b5e96a6b185a06e2b85e73052d1f5bbbe
Your Name <you@example.com>
2025-11-11 14:56:55 +0100

    feat: handle readonly prop / add transitions

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
 const value = defineModel<'in-progress' | 'completed'>()
+const emit = defineEmits(['commit'])
+
+const { readonly = false } = defineProps<{
+  readonly?: boolean
+}>()
+
+const toggleValue = () => {
+  if (readonly) return
+  value.value = value.value === 'completed' ? 'in-progress' : 'completed'
+  emit('commit')
+}
 </script>
 
 <template>
-  <div class="text-xl cursor-pointer">
-    <iconify-icon v-if="value === 'completed'" icon="lucide:circle-check" class="text-green-500" />
-    <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
+  <div class="text-xl" :class="{ 'cursor-pointer': !readonly }" @click="toggleValue">
+    <Transition mode="out-in">
+      <iconify-icon
+        v-if="value === 'completed'"
+        icon="lucide:circle-check"
+        class="text-green-500"
+        key="completed"
+      />
+      <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" key="in-progress" />
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: transform 0.1s;
+}
+.v-enter-from,
+.v-leave-to {
+  transform: scale(0.369);
+}
+</style>
