d0bb9fa51c1fb1e523a31674f7ee0505f0f67e16
Your Name <you@example.com>
2025-11-17 16:29:08 +0100

    chore: setup formkit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { plugin } from '@formkit/vue'
+import config from '../formkit.config'
 import './assets/main.css'
 import 'iconify-icon'
 import App from './App.vue'
@@ -9,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(plugin, config)
 
 app.mount('#app')
