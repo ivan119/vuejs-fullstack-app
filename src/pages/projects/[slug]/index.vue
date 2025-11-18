1301490768fdc4a0dd24d60464ab90d4ecb4a1a7
Your Name <you@example.com>
2025-11-18 20:29:00 +0100

    feat: page transitions

--- src/pages/projects/[slug]/index.vue
+++ src/pages/projects/[slug]/index.vue
@@ -17,106 +17,111 @@ onMounted(() => {
 })
 </script>
 <template>
-  <section v-if="project">
-    <Table>
-      <TableRow>
-        <TableHead> Name </TableHead>
-        <TableCell>
-          <AppInPlaceEditText v-model="project.name" @commit="updateProject" />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableHead> Project Description </TableHead>
-        <TableCell>
-          <AppInPlaceEditTextarea v-model="project.description" @commit="updateProject" />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableHead> Status </TableHead>
-        <TableCell
-          ><AppInPlaceEditStatus v-model="project.status" @commit="updateProject"
-        /></TableCell>
-      </TableRow>
-      <TableRow>
-        <TableHead> Collaborators </TableHead>
-        <TableCell>
-          <div class="flex">
-            <Avatar
-              v-for="collab in collabs"
-              :key="collab.id"
-              class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            >
-              <RouterLink
-                :to="{ name: '/users/[username]', params: { username: collab.username } }"
-                class="w-full h-full flex items-center justify-center"
+  <div>
+    <section v-if="project">
+      <Table>
+        <TableRow>
+          <TableHead> Name </TableHead>
+          <TableCell>
+            <AppInPlaceEditText v-model="project.name" @commit="updateProject" />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableHead> Project Description </TableHead>
+          <TableCell>
+            <AppInPlaceEditTextarea v-model="project.description" @commit="updateProject" />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableHead> Status </TableHead>
+          <TableCell
+            ><AppInPlaceEditStatus v-model="project.status" @commit="updateProject"
+          /></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableHead> Collaborators </TableHead>
+          <TableCell>
+            <div class="flex">
+              <Avatar
+                v-for="collab in collabs"
+                :key="collab.id"
+                class="-mr-4 border border-primary hover:scale-110 transition-transform"
               >
-                <AvatarImage :src="collab.avatar_url || ''" :alt="collab.avatar_url || collab.id" />
-                <AvatarFallback> </AvatarFallback>
-              </RouterLink>
-            </Avatar>
-          </div>
-        </TableCell>
-      </TableRow>
-    </Table>
-
-    <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
-      <div class="flex-1">
-        <h2>Tasks</h2>
-        <div class="table-container">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead> Name </TableHead>
-                <TableHead> Status </TableHead>
-                <TableHead> Due Date </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow v-for="task in project.tasks" :key="task.id">
-                <TableCell class="p-0">
-                  <RouterLink
-                    class="text-left block hover:bg-muted p-4"
-                    :to="{ name: '/tasks/[id]/', params: { id: task.id } }"
-                  >
-                    {{ task.name }}
-                  </RouterLink></TableCell
+                <RouterLink
+                  :to="{ name: '/users/[username]', params: { username: collab.username } }"
+                  class="w-full h-full flex items-center justify-center"
                 >
-                <TableCell>
-                  <AppInPlaceEditStatus v-model="task.status" readonly />
-                </TableCell>
-                <TableCell> {{ task.due_date }} </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+                  <AvatarImage
+                    :src="collab.avatar_url || ''"
+                    :alt="collab.avatar_url || collab.id"
+                  />
+                  <AvatarFallback> </AvatarFallback>
+                </RouterLink>
+              </Avatar>
+            </div>
+          </TableCell>
+        </TableRow>
+      </Table>
+
+      <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+        <div class="flex-1">
+          <h2>Tasks</h2>
+          <div class="table-container">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead> Name </TableHead>
+                  <TableHead> Status </TableHead>
+                  <TableHead> Due Date </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow v-for="task in project.tasks" :key="task.id">
+                  <TableCell class="p-0">
+                    <RouterLink
+                      class="text-left block hover:bg-muted p-4"
+                      :to="{ name: '/tasks/[id]/', params: { id: task.id } }"
+                    >
+                      {{ task.name }}
+                    </RouterLink></TableCell
+                  >
+                  <TableCell>
+                    <AppInPlaceEditStatus v-model="task.status" readonly />
+                  </TableCell>
+                  <TableCell> {{ task.due_date }} </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
         </div>
-      </div>
-      <div class="flex-1">
-        <h2>Documents</h2>
-        <div class="table-container">
-          <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
-            This project doesn't have documents yet...
-          </p>
-          <!-- <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead> Name </TableHead>
-                <TableHead> Visibility </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell> Lorem ipsum dolor sit amet. </TableCell>
-                <TableCell> Private </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table> -->
+        <div class="flex-1">
+          <h2>Documents</h2>
+          <div class="table-container">
+            <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
+              This project doesn't have documents yet...
+            </p>
+            <!-- <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead> Name </TableHead>
+                  <TableHead> Visibility </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+                  <TableCell> Private </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table> -->
+          </div>
         </div>
-      </div>
+      </section>
     </section>
-  </section>
+  </div>
 </template>
 
-<style>
+<style scoped>
 @reference '@/assets/main.css'
 th {
   @apply w-[100px];
